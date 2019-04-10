cb9cc4a4ed6ef3d7b5a4bff286d73d1f65bd2e1d
mpkuse <mpkuse@connect.ust.hk>
2019-04-10 16:09:02 +0800

    added maps of hanghau-eastpointcity and a way to specify map on commandline

--- src/image_to_rviz_node.cpp
+++ src/image_to_rviz_node.cpp
@@ -7,6 +7,7 @@
 #include <opencv/cv.hpp>
 #include <dynamic_reconfigure/server.h>
 #include <image_to_rviz/paramsConfig.h>
+#include <string>
 
 double lu_x = 0, lu_y = 0;
 double yaw = -3.14;
@@ -14,9 +15,9 @@ double sizeSq = 1;
 double z = -10;
 
 void callback(image_to_rviz::paramsConfig &config, uint32_t level) {
-  ROS_INFO("Reconfigure Request: %lf %lf %lf %lf %lf", 
-            config.size_param, config.dx_param, 
-            config.dy_param, 
+  ROS_INFO("Reconfigure Request: %lf %lf %lf %lf %lf",
+            config.size_param, config.dx_param,
+            config.dy_param,
             config.yaw,
             config.z);
 
@@ -43,8 +44,30 @@ int main( int argc, char** argv )
 
       cv::Mat src;
       std::string packagePath = ros::package::getPath("image_to_rviz");
-      src = cv::imread(packagePath + "/img/cyt.png", 1 );
-      //cv::resize(src, src, cv::Size(src.cols / 4, src.rows / 4));
+      std::string fname = "/img/cyt.png";
+      if( argc == 1 ) {
+          std::cout << "No image name specified, so using default image.\n";
+          std::cout << "Invalid Usage. Sample Usage:\n$ " << argv[0] << " <image name> \n";
+          std::cout << "image name need to be relative to this package.\n";
+
+      } else if( argc == 2 ) {
+          std::cout << "Will try to use specified image name: " << argv[1] << " \n";
+          fname = std::string( argv[1] );
+      } else {
+          std::cout << "Invalid Usage. Sample Usage:\n$ " << argv[0] << " <image name> \n";
+          exit(1);
+      }
+
+      std::cout << "Open Image : " << packagePath + "/" + fname << std::endl;
+      src = cv::imread(packagePath + "/" + fname, 1 );
+
+      if( src.empty() ) {
+          std::cout << "[ERROR] Cannot open image file. Most likely the image, the path you specified did not exist or there are permission issue.\n";
+          exit(1);
+      }
+    //   src = cv::imread(packagePath + "/img/cyt.png", 1 );
+      cv::resize(src, src, cv::Size(src.cols / 2, src.rows / 2));
+    std::cout << "Image was successfully opened...\n";
 
       visualization_msgs::Marker image;
       image.header.frame_id = "/world";
@@ -86,7 +109,7 @@ int main( int argc, char** argv )
                         p.y = lu_y + (x * sin(yaw) + y * cos(yaw)) * pix;
                         image.points.push_back(p);
                         image.colors.push_back(crgb);
-                        
+
                         x = (r - center_x) + 1;
                         y = (c - center_y);
                         p.x = lu_x + (x * cos(yaw) - y * sin(yaw)) * pix;
@@ -121,13 +144,13 @@ int main( int argc, char** argv )
                         p.y = lu_y + (x * sin(yaw) + y * cos(yaw)) * pix;
                         image.points.push_back(p);
                         image.colors.push_back(crgb);
-                        
+
                       }
                   }
 
             marker_pub.publish(image);
             ros::spinOnce();
             r.sleep();
-            
+
       }
 }
